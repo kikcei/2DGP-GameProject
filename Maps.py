bfe1d323642b8a09dc542b28d866c45cae689292
abg <kikceivieov@gmail.com>
2025-10-06 03:52:34 +0900

    map 기본 클래스 설계

---
+++ Maps.py
@@ -0,0 +1,22 @@
+from pico2d import *
+
+class Sky:
+    def __init__(self):
+        self.image = load_image('resources/maps/images/1391.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300)
+        pass
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('resources/maps/images/DefineSprite_1860.png')
+    def update(self):    #객체의 상호 작용, 행위
+        pass
+
+    def draw(self):
+        self.image.draw(400,30)
+        pass
