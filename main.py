89724b4b2295ad7b641ae82170473972ae4f90c3
abg <kikceivieov@gmail.com>
2025-10-08 03:27:19 +0900

    player main 출력

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from pico2d import *
 from Maps import *
+from Player import *
+
 WIDTH, HEIGHT = 800,600
 
 def main():
     open_canvas(WIDTH, HEIGHT)
 
     map = Maps()
+    player = Player()
+
     while True:
         clear_canvas()  # 이전 프레임 지움
 
         map.draw()  # 지도
+        player.update()  # 플레이어 상태 업데이트
+        player.draw()  # 플레이어 그리기
 
         update_canvas()  # 그린 걸 실제로 화면에 표시
 
-        delay(0.03)
+        delay(0.04)
 
     close_canvas()
 
