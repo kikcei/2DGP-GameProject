f5215caa0cb5bfbd33878f3a4b37f20ae0b1fec5
abg <kikceivieov@gmail.com>
2025-11-10 20:56:50 +0900

    이미지 로드 및 좌 우 DRAW 수정

--- Player.py
+++ Player.py
@@ -1,6 +1,6 @@
 from pico2d import *
 from state_machine import StateMachine
-
+from resource_load import PlayerResourceLoad
 import time
 
 DOUBLE_TAP_RUN_TIME = 0.2
@@ -46,9 +46,12 @@ class Idle:
         self.player.frame_players_stop_leg = (self.player.frame_players_stop_leg + 1) % 11
 
     def draw(self):
-        self.player.image_players_stop_leg.draw(self.player.x - 3, self.player.y - 56)  # Adjust y position for leg
-        self.player.image_players_stop_body[self.player.frame_players_stop_body].draw(self.player.x, self.player.y)
-
+        if self.player.face_dir == 1:
+            self.player.image_players_stop_leg.draw(self.player.x - 3, self.player.y - 56)  # Adjust y position for leg
+            self.player.image_players_stop_body[self.player.frame_players_stop_body].draw(self.player.x, self.player.y)
+        else:
+            self.player.image_players_stop_leg_left.draw(self.player.x - 3, self.player.y - 56)  # Adjust y position for leg
+            self.player.image_players_stop_body_left[self.player.frame_players_stop_body].draw(self.player.x, self.player.y)
 
 
 class Walk:
@@ -88,17 +91,10 @@ class Walk:
 
     def draw(self):
         # 방향에 따라 그리기
-        if self.player.face_dir >= 1:
+        if self.player.face_dir == 1:
             self.player.image_players_walk[self.player.frame_players_walk].draw(self.player.x, self.player.y)
-        elif self.player.face_dir <= -1:
-            self.player.image_players_walk[self.player.frame_players_walk].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0 and self.player.keys['left'] and self.player.keys['up']:
-            self.player.image_players_walk[self.player.frame_players_walk].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0 and self.player.keys['right'] and self.player.keys['down']:
-            self.player.image_players_walk[self.player.frame_players_walk].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0:
-            self.player.image_players_stop_leg.draw(self.player.x - 3, self.player.y - 56)
-            self.player.image_players_stop_body[self.player.frame_players_stop_body].draw(self.player.x, self.player.y)
+        else:
+            self.player.image_players_walk_left[self.player.frame_players_walk].draw(self.player.x, self.player.y)
 
 
 
@@ -133,22 +129,15 @@ class  Run:
             self.player.frame_players_stop_leg = (self.player.frame_players_stop_leg + 1) % 11
 
         # 위치 갱신
-        self.player.x += self.player.dirx * 5
-        self.player.y += self.player.diry * 5
+        self.player.x += self.player.dirx * 10
+        self.player.y += self.player.diry * 10
 
     def draw(self):
         # 방향에 따라 그리기
-        if self.player.face_dir >= 1:
-            self.player.image_players_run[self.player.frame_players_run].draw(self.player.x, self.player.y)
-        elif self.player.face_dir <= -1:
-            self.player.image_players_run[self.player.frame_players_run].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0 and self.player.keys['left'] and self.player.keys['up']:
-            self.player.image_players_run[self.player.frame_players_run].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0 and self.player.keys['right'] and self.player.keys['down']:
-            self.player.image_players_run[self.player.frame_players_run].draw(self.player.x, self.player.y)
-        elif self.player.face_dir == 0:
-            self.player.image_players_stop_leg.draw(self.player.x - 3, self.player.y - 56)
-            self.player.image_players_stop_body[self.player.frame_players_stop_body].draw(self.player.x, self.player.y)
+        if self.player.face_dir == 1:
+            self.player.image_players_run[self.player.frame_players_run].draw(self.player.x, self.player.y - 10)
+        else:
+            self.player.image_players_run_left[self.player.frame_players_run].draw(self.player.x, self.player.y - 10)
 
 
 class Attack:
@@ -172,17 +161,68 @@ class Attack:
         self.player.frame_players_attack_a = (self.player.frame_players_attack_a + 1) % 8
 
     def draw(self):
-        self.player.image_players_attack_a[self.player.frame_players_attack_a].draw(self.player.x + 22, self.player.y - 8)
+        if self.player.face_dir == 1:
+            self.player.image_players_attack_a[self.player.frame_players_attack_a].draw(self.player.x + 22,self.player.y - 8)
+        else:
+            self.player.image_players_attack_a_left[self.player.frame_players_attack_a].draw(self.player.x + 22,self.player.y - 8)
 
 
 
 class Player:
 
-    def __init__(self):
-        self.image_players_stop_body = []
-        self.image_players_walk = []
-        self.image_players_run = []
-        self.image_players_attack_a = []
+    def __init__(self,resource_loader: PlayerResourceLoad):
+        self.image_players_shadow = resource_loader.get('shadow')
+
+        # 정지 상태
+        self.image_players_stop_body = resource_loader.get('stop_body')
+        self.image_players_stop_leg = resource_loader.get('stop_leg')
+        self.image_players_stop_body_left = resource_loader.get('stop_body_left')
+        self.image_players_stop_leg_left = resource_loader.get('stop_leg_left')
+
+        # 걷기
+        self.image_players_walk = resource_loader.get('walk')
+        self.image_players_walk_left = resource_loader.get('walk_left')
+
+        # 달리기
+        self.image_players_run = resource_loader.get('run')
+        self.image_players_run_left = resource_loader.get('run_left')
+
+        # 공격 (A)
+        self.image_players_attack_a = resource_loader.get('attack_a')
+        self.image_players_attack_a_left = resource_loader.get('attack_a_left')
+
+        # 공격 (A+A)
+        self.image_players_attack_a_a = resource_loader.get('attack_a_a')
+        self.image_players_attack_a_a_left = resource_loader.get('attack_a_a_left')
+
+        # 공격 (A+S)
+        self.image_players_attack_a_s = resource_loader.get('attack_a_s')
+        self.image_players_attack_a_s_left = resource_loader.get('attack_a_s_left')
+
+        # 공격 (A+S+A)
+        self.image_players_attack_a_s_a = resource_loader.get('attack_a_s_a')
+        self.image_players_attack_a_s_a_left = resource_loader.get('attack_a_s_a_left')
+
+        # 공격 (S)
+        self.image_players_attack_s = resource_loader.get('attack_s')
+        self.image_players_attack_s_left = resource_loader.get('attack_s_left')
+
+        # 공격 (S+S)
+        self.image_players_attack_s_s = resource_loader.get('attack_s_s')
+        self.image_players_attack_s_s_left = resource_loader.get('attack_s_s_left')
+
+        self.frame_players_stop_body = 0
+        self.frame_players_stop_leg = 0
+        self.frame_players_walk = 0
+        self.frame_players_run = 0
+        self.frame_players_attack_a = 0
+        self.frame_players_attack_a_a = 0
+        self.frame_players_attack_a_s = 0
+        self.frame_players_attack_a_s_a = 0
+        self.frame_players_attack_s = 0
+        self.frame_players_attack_s_s = 0
+
+
         self.dirx = 0
         self.diry = 0
 
@@ -193,43 +233,6 @@ class Player:
         self.last_key_time = {'left': 0, 'right': 0, 'up': 0, 'down': 0}
         self.last_tap_time = 0
 
-
-        # stop
-        for i in range(1, 12):
-            path = f'resources/Players/sprites/stop/DefineSprite_60/{i}.png'
-            image = load_image(path)
-            self.image_players_stop_body.append(image)
-        # walk
-        for i in range(1, 11):
-            path = f'resources/Players/sprites/walk/DefineSprite_79/{i}.png'
-            image = load_image(path)
-            self.image_players_walk.append(image)
-        # run
-        for i in range(1, 6):
-            path = f'resources/Players/sprites/run/DefineSprite_91/{i}.png'
-            image = load_image(path)
-            self.image_players_run.append(image)
-        # attack_a
-        for i in range(1, 9):
-            path = f'resources/Players/sprites/attack_a/DefineSprite_97/{i}.png'
-            image = load_image(path)
-            self.image_players_attack_a.append(image)
-
-        self.image_players_shadow = load_image('resources/Players/sprites/shadow/DefineSprite_45/1.png')
-        self.image_players_stop_leg = load_image('resources/Players/sprites/stop/DefineSprite_124/1.png')
-
-        self.player_stop_body_frame_max = 11
-        self.player_stop_leg_frame_max = 11
-        self.player_walk_frame_max = 11
-        self.player_run_frame_max = 6
-        self.player_attack_a_frame_max = 9
-
-
-        self.frame_players_stop_body = 0
-        self.frame_players_stop_leg = 0
-        self.frame_players_walk = 0
-        self.frame_players_run = 0
-        self.frame_players_attack_a = 0
 
         self.IDLE = Idle(self)
         self.WALK = Walk(self)
@@ -244,7 +247,9 @@ class Player:
                 self.WALK: {  right_down: self.WALK, left_down: self.WALK,
                              up_down: self.WALK,  down_down: self.WALK, down_a: self.ATTACK},
 
-                self.ATTACK: {}
+                self.ATTACK: {},
+
+                self.RUN: {down_a: self.ATTACK}
             }
         )
 
@@ -255,7 +260,9 @@ class Player:
 
         if evt.type == SDL_KEYDOWN:
             if evt.key == SDLK_LEFT: self.keys['left'] = True
+            self.face_dir = -1
             if evt.key == SDLK_RIGHT: self.keys['right'] = True
+            self.face_dir = 1
             if evt.key == SDLK_UP: self.keys['up'] = True
             if evt.key == SDLK_DOWN: self.keys['down'] = True
 
