290f4af37ecbbbf73b380d694d1d2c5a6b06f83d
abg <kikceivieov@gmail.com>
2025-11-19 00:56:38 +0900

    game_world 기본 틀 구현

---
+++ game_world.py
@@ -0,0 +1,23 @@
+# world[0] : 가장 낮은 계층 layer
+# world[1] : 그 위의 계층 layer
+
+world = []
+
+def add_object(o,depth):
+    world[depth].append(o)
+
+def update_world():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def draw_world():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
